d53d1a3ab880bbe12af4668be21bbee1f274359e
HenrikJ <henrikpoulsen_87@hotmail.com>
2018-05-29 13:34:08 +0200

    Add project files.

---
+++ BatchModuleResultMonitor/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace BatchModuleResultMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.InfoText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.DimGray;
+            this.chart1.BorderlineColor = System.Drawing.Color.DarkGray;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.DarkGray;
+            chartArea2.BackColor = System.Drawing.Color.Gray;
+            chartArea2.BackSecondaryColor = System.Drawing.Color.DarkGray;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(47, 138);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.LabelBackColor = System.Drawing.Color.Gray;
+            series2.Legend = "Legend1";
+            series2.Name = "Batch Weight";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1064, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 553);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InfoText
+            // 
+            this.InfoText.Location = new System.Drawing.Point(180, 58);
+            this.InfoText.Name = "InfoText";
+            this.InfoText.Size = new System.Drawing.Size(931, 20);
+            this.InfoText.TabIndex = 2;
+            this.InfoText.TextChanged += new System.EventHandler(this.InfoText_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(75, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Application info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1523, 863);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InfoText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox InfoText;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BatchModuleResultMonitor/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatchModuleResultMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            string SerieName = "Batch Weight";
+            for (int i = 0; i < 10; i++)
+            {
+                this.chart1.Series[SerieName].Points.AddXY(i.ToString(), 200);
+
+            }
+            /*
+            this.chart1.Series[SerieName].Points.AddXY("1131 - 1160", 220);
+            this.chart1.Series[SerieName].Points.AddXY("1161 - 1190", 230);
+            this.chart1.Series[SerieName].Points.AddXY("1191 - 1220", 250);
+            this.chart1.Series[SerieName].Points.AddXY("1221 - 1250", 200);
+            this.chart1.Series[SerieName].Points.AddXY("1251 - 1280", 270);
+            this.chart1.Series[SerieName].Points.AddXY("1281 - 1310", 200);
+            */
+            InfoText.Text = "Data loaded correct";
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            // Set 
+            InfoText.Text = " Series 1 data";
+        }
+
+        private void InfoText_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
