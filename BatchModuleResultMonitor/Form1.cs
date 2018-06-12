3ce3764ea909912f7b9366954f29e6fb37b34a91
HenrikJ <henrikpoulsen_87@hotmail.com>
2018-06-12 08:37:06 +0200

    First build

--- BatchModuleResultMonitor/Form1.cs
+++ BatchModuleResultMonitor/Form1.cs
@@ -21,25 +21,38 @@ namespace BatchModuleResultMonitor
         private void button1_Click(object sender, EventArgs e)
 
         {
+
+            //********************************************************************************************************
+            //  Read to string
+            //********************************************************************************************************
+            string[] s3 = File.ReadAllLines(@"C:\Temp\Test.txt");
+            int[] iData = new int[s3.Length];
+
+            // convert to int
+            for (int i = 0; i < s3.Length; i++)
+            {
+                iData[i] = int.Parse(s3[i]);
+            }
+
+            // Sort numbers
+            Array.Sort(iData);
+
+
             //********************************************************************************************************
             // Add data to barchart
             //********************************************************************************************************
             string SerieName = "Batch Weight";
             int NumberOfBar = 40;
             this.chart1.Series[SerieName].Points.Clear();
-            for (int i = 0; i < NumberOfBar; i++)
+            for (int i = 0; i < iData.Length; i++)
             {
-                chart1.Series[SerieName].Points.AddXY((i+1).ToString() , 200);
+                chart1.Series[SerieName].Points.AddXY((i + 1).ToString(), iData[i]);
+             
             }
             // Add text when file load done
             InfoText.Text = "Data loaded correct";
 
 
-
-            //********************************************************************************************************
-            //  Read to string
-            //********************************************************************************************************
-            string[] s3 = File.ReadAllLines(@"C:\Temp\Test.txt");
 
 
             //********************************************************************************************************
@@ -50,13 +63,17 @@ namespace BatchModuleResultMonitor
         private void chart1_Click(object sender, EventArgs e)
         {
             // Set 
-            InfoText.Text = " Series 1 data";
+            InfoText.Text = " Series 1 data ";
         }
         
         private void InfoText_TextChanged(object sender, EventArgs e)
         {
           
         }
-        
+   
+
     }
+
+   
+
 }
